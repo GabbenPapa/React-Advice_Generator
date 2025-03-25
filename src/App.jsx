9e48ef4f41983122833aca3d19a4bbb10b107f2f
Bozzay Gábor <111114673+GabbenPapa@users.noreply.github.com>
2025-03-25 16:39:18 +0100

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,35 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 export default function App() {
   const [advice, setAdvice] = useState("");
-	const [count, setCount ] = useState(0);
+  const [count, setCount] = useState(0);
 
   async function getAdvice() {
-		const response = await fetch('https://api.adviceslip.com/advice');
-		const data = await response.json();
-		setAdvice(data.slip.advice);
-		setCount(count + 1);
-	}
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    setAdvice(data.slip.advice);
+    setCount(count + 1);
+  }
 
-	useEffect( function() {
-		getAdvice();
-	}, []);
+  useEffect(function () {
+    getAdvice();
+  }, []);
 
   return (
     <>
       <div>
-				<h1>{advice}</h1>
-				<button onClick={getAdvice}> Get advice</button>
-				<Message count={count}/>
+        <h1>{advice}</h1>
+        <button onClick={getAdvice}>Get advice</button>
+        <Message count={count} />
       </div>
     </>
-  )
+  );
 }
 
-
 function Message(props) {
-	return (
-		<p>
-			You have read <strong>{props.count}</strong> pices of advice 
-		</p>
-	);
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> pices of advice
+    </p>
+  );
 }
